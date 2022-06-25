81c650f2e1a3e7c995b782c3e3e94ecd4ed2d055
GuojianZou <32932644+zouguojian@users.noreply.github.com>
2022-06-25 17:15:41 +0800

    first

--- model/multi_convlstm.py
+++ model/multi_convlstm.py
@@ -3,7 +3,7 @@ from model.convlstm import ConvLSTMCell
 import tensorflow as tf
 
 class mul_convlstm(object):
-    def __init__(self, batch, predict_time, shape=[3, 3], filters= 32 , kernel=[32, 2], layer_num=1, activation=tf.tanh, normalize=True, reuse=None):
+    def __init__(self, batch, predict_time, shape=[162, 5], filters= 32 , kernel=[162, 2], layer_num=1, activation=tf.tanh, normalize=True, reuse=None):
         self.batch=batch
         self.predict_time=predict_time
         self.layers=layer_num
@@ -15,8 +15,6 @@ class mul_convlstm(object):
         self.shape = shape
         self.kernel = kernel
         self.filters = filters        # numbers of output channel
-
-
 
     def encoding(self, inputs):
         '''
@@ -32,11 +30,11 @@ class mul_convlstm(object):
             '''
             inputs shape is : [batch size, time size, site number, features, input channel]
             outputs is : [batch size, time size, site number, features, output channel]
-            state: LSTMStateTuple(c=<tf.Tensor 'rnn/while/Exit_3:0' shape=(32, 162, 5, 12) dtype=float32>,
+            state: LSTMStateTuple(c=<tf.Tensor 'rnn/while/Exit_3:0' shape=(32, 162, 5, 12) dtype=float32>, 
                     h=<tf.Tensor 'rnn/while/Exit_4:0' shape=(32, 162, 5, 12) dtype=float32>)
             '''
-            # init_state=cell.zero_state(self.batch,tf.float32)
-            outputs, state = tf.nn.dynamic_rnn(cell, inputs, dtype=tf.float32)
+            init_state=cell.zero_state(self.batch,tf.float32)
+            outputs, state = tf.nn.dynamic_rnn(cell, inputs, initial_state=init_state, dtype=inputs.dtype)
 
             print(outputs.shape)
             print(state)
@@ -51,37 +49,35 @@ class mul_convlstm(object):
         h = []
         h_state = encoder_hs[:, -1, :, :, :]
         h_state = tf.expand_dims(input=h_state, axis=1)
-
-        '''
-        inputs shape is [batch size, 1, site num, features, out channel)
-        '''
-        # Add the ConvLSTM step.
-        with tf.variable_scope(name_or_scope='decoder_convlstm', reuse=tf.AUTO_REUSE):
+        with tf.variable_scope(name_or_scope='decoder_convlstm',reuse=tf.AUTO_REUSE):
+            '''
+            inputs shape is [batch size, 1, site num, features, out channel)
+            '''
+            # Add the ConvLSTM step.
             cell = ConvLSTMCell(self.shape, self.filters, self.kernel)
-            # initial_state = cell.zero_state(self.batch, tf.float32)
+            init_state = cell.zero_state(self.batch, tf.float32)
 
             '''
             inputs shape is : [batch size, 1, site number, features, input channel]
             outputs is : [batch size, 1, site number, features, output channel]
-            state: LSTMStateTuple(c=<tf.Tensor 'rnn/while/Exit_3:0' shape=(32, 162, 5, 12) dtype=float32>,
+            state: LSTMStateTuple(c=<tf.Tensor 'rnn/while/Exit_3:0' shape=(32, 162, 5, 12) dtype=float32>, 
                     h=<tf.Tensor 'rnn/while/Exit_4:0' shape=(32, 162, 5, 12) dtype=float32>)
             '''
             for i in range(self.predict_time):
                 ''' return shape is [batch size, time size, height, site num, out channel) '''
-                # # with tf.variable_scope('decoder_lstm', reuse=tf.AUTO_REUSE):
-                h_state, state = tf.nn.dynamic_rnn(cell=cell, inputs=h_state, dtype=tf.float32)
-                # # initial_state = state
-                #
-                # print(h_state.shape)
+                with tf.variable_scope('decoder_lstm', reuse=tf.AUTO_REUSE):
+                    h_state, state = tf.nn.dynamic_rnn(cell=cell, inputs=h_state,
+                                                        initial_state=init_state, dtype=tf.float32)
+                init_state = state
 
-                max_pool = tf.nn.avg_pool(tf.squeeze(h_state,axis=1), ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME', name='lstm-pooling')
+                max_pool = tf.nn.avg_pool(tf.squeeze(h_state,axis=1), ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
                 cnn_shape = max_pool.get_shape().as_list()
                 nodes = cnn_shape[1] * cnn_shape[2] * cnn_shape[3]
-                results = tf.layers.dense(inputs=tf.reshape(max_pool, [cnn_shape[0], nodes]), units=1, name='lstm-layer', reuse=tf.AUTO_REUSE)
-
+                results = tf.layers.dense(inputs=tf.reshape(max_pool, [cnn_shape[0], nodes]), units=256, name='layer_1', reuse=tf.AUTO_REUSE,activation=tf.nn.relu)
+                results = tf.layers.dense(inputs=results, units=1, name='layer_2', reuse=tf.AUTO_REUSE)
                 h.append(results)
-        pre=tf.concat(h,axis=1)
-        return pre
+            pre=tf.concat(h,axis=1)
+            return pre
 
 if __name__ == '__main__':
 
@@ -92,12 +88,12 @@ if __name__ == '__main__':
     channels = 1
     filters = 12        # numbers of output channel
 
-    # # Create a placeholder for videos.
-    # inputs = tf.placeholder(tf.float32, [batch_size, timesteps] + shape)
-    #
-    # multi=mul_convlstm(batch=32, predict_time=2)
-    #
-    # hs=multi.encoding(inputs)
-    # print(hs.shape)
-    # pre =multi.decoding(hs)
-    # print(pre.shape)
+    # Create a placeholder for videos.
+    inputs = tf.placeholder(tf.float32, [batch_size, timesteps] + shape)
+
+    multi=mul_convlstm(batch=32, predict_time=2)
+
+    hs=multi.encoding(inputs)
+    print(hs.shape)
+    pre =multi.decoding(hs)
+    print(pre.shape)
--- model/res_net.py
+++ model/res_net.py
@@ -1,19 +1,19 @@
 # -- coding: utf-8 --
 import tensorflow as tf
-tf.reset_default_graph()
+
+
 class resnet(object):
-    def __init__(self, batch_size, para=None):
+    def __init__(self, para=None):
         '''
         :param batch_size:
         :param para:
         '''
-        self.batch_size=batch_size
-        self.para=para
-        self.h=[2,3,2]
-        self.w=[2,3,2]
-        self.out_channel=[3,3,3]
-        self.in_channel=[1]+self.out_channel
-        self.features=5
+        self.para = para
+        self.h = [3, 3, 3]
+        self.w = [3, 3, 3]
+        self.out_channel = [3, 3, 3]
+        self.in_channel = [1] + self.out_channel
+        self.features = 5
 
     def conv_2d(self, x, h, w, in_channel, out_channel, layer_name):
         '''
@@ -24,16 +24,19 @@ class resnet(object):
         :param out_channel:
         :return:
         '''
-        filter=tf.Variable(initial_value=tf.random_normal(shape=[h, w, in_channel, out_channel]),name=layer_name)
-        layer=tf.nn.conv2d(input=x,filter=filter,strides=[1,1,1,1],padding='SAME')
-        return layer
+        filter = tf.Variable(initial_value=tf.truncated_normal(shape=[h, w, in_channel, out_channel], stddev=0.1),
+                             name=layer_name)
+        # filter = tf.get_variable(name=layer_name, shape=[h, w, in_channel, out_channel],initializer=tf.truncated_normal_initializer(stddev=0.1))
+        bias = tf.get_variable(layer_name + "/bias", [out_channel], initializer=tf.constant_initializer(0))
+        layer = tf.nn.conv2d(input=x, filter=filter, strides=[1, 1, 1, 1], padding='SAME')
+        return tf.nn.bias_add(layer, bias)
 
     def relu(self, inputs):
         '''
         :param inputs:
         :return:
         '''
-        relu=tf.nn.relu(inputs)
+        relu = tf.nn.relu(inputs)
         return relu
 
     def block(self, x, in_channel, out_channel, block_name):
@@ -44,11 +47,11 @@ class resnet(object):
         :param block_name:
         :return:
         '''
-        x1 = self.conv_2d(x, self.h[0], self.w[0], in_channel[0], out_channel[0], block_name.join('1'))
+        x1 = self.conv_2d(x, self.h[0], self.w[0], in_channel[0], out_channel[0], block_name + '/1')
         x1 = self.relu(x1)
-        x2 = self.conv_2d(x1, self.h[0], self.w[0], in_channel[1], out_channel[1], block_name.join('2'))
+        x2 = self.conv_2d(x1, self.h[0], self.w[0], in_channel[1], out_channel[1], block_name + '/2')
         x2 = self.relu(x2)
-        x3 = self.conv_2d(x2, self.h[0], self.w[0], in_channel[2], out_channel[2], block_name.join('3'))
+        x3 = self.conv_2d(x2, self.h[0], self.w[0], in_channel[2], out_channel[2], block_name + '/3')
         x3 = self.relu(x3)
         return x3
 
@@ -64,36 +67,38 @@ class resnet(object):
         :return:
         '''
         filter = tf.Variable(initial_value=tf.random_normal(shape=[h, w, in_channel, out_channel]), name=residual_name)
-        conv = tf.nn.conv2d(x1, filter, strides=[1, 1, 1, 1],padding='SAME')
+        bias = tf.get_variable(residual_name + "/bias", [out_channel], initializer=tf.constant_initializer(0))
+        conv = tf.nn.conv2d(x1, filter, strides=[1, 1, 1, 1], padding='SAME')
+        conv = tf.nn.bias_add(conv, bias)
         layer_add = conv + x2
-        return layer_add
+        return self.relu(layer_add)
 
-    def cnn(self,x):
+    def cnn(self, x):
         '''
         :param x: [batch size, site num, features, channel]
         :return: [batch size, height, channel]
         '''
 
-        with tf.variable_scope(name_or_scope='resnet',reuse=tf.AUTO_REUSE):
+        with tf.variable_scope(name_or_scope='resnet', reuse=tf.AUTO_REUSE):
+            block1 = self.block(x, [1, 3, 3], [3, 3, 3], block_name='block1')
+            residual1 = self.residual_connected(x, block1, 1, 1, 1, 3, residual_name='residual1')
+            print('residual 1 shape is : ', residual1.shape)
 
-            block1=self.block(x,[1,8,8],[8,8,8],block_name='block1')
-            residual1=self.residual_connected(x,block1,1,1,1,8,residual_name='residual1')
-            print('residual 1 shape is : ',residual1.shape)
+            block2 = self.block(residual1, [3, 6, 6], [6, 6, 6], block_name='block2')
+            residual2 = self.residual_connected(residual1, block2, 1, 1, 3, 6, residual_name='residual2')
+            print('residual 2 shape is : ', residual2.shape)
 
-            block2=self.block(residual1,[8,16,16],[16,16,16],block_name='block2')
-            residual2=self.residual_connected(residual1,block2,1,1,8,16,residual_name='residual2')
-            print('residual 2 shape is : ',residual2.shape)
+            block3 = self.block(residual2, [6, 6, 6], [6, 6, 6], block_name='block3')
+            residual3 = self.residual_connected(residual2, block3, 1, 1, 6, 6, residual_name='residual3')
+            print('residual 3 shape is : ', residual3.shape)
 
-            block3=self.block(residual2,[16,32,32],[32,32,32],block_name='block3')
-            residual3=self.residual_connected(residual2,block3,1,1,16,32,residual_name='residual3')
-            print('residual 3 shape is : ',residual3.shape)
+            block4 = self.block(residual3, [6, 6, 6], [6, 6, 6], block_name='block4')
+            residual4 = self.residual_connected(residual3, block4, 1, 1, 6, 6, residual_name='residual4')
+            print('residual 4 shape is : ', residual4.shape)
 
-            block4=self.block(residual3,[32,32,32],[32,32,32],block_name='block4')
-            residual4=self.residual_connected(residual3,block4,1,1,32,32,residual_name='residual4')
-            print('residual 4 shape is : ',residual4.shape)
+            # max_pool=tf.nn.avg_pool(residual4, ksize=[1, 2, 2, 1], strides=[1, 1, 2, 1], padding='SAME')
 
-            max_pool=tf.nn.avg_pool(residual4, ksize=[1, 2, 2, 1], strides=[1, 1, 2, 1], padding='SAME', name='cnn-pooling')
-
+            max_pool = residual4
             print('max_pool output shape is : ', max_pool.shape)
 
         # cnn_shape = max_pool3.get_shape().as_list()
@@ -102,17 +107,17 @@ class resnet(object):
 
         return max_pool
 
-if __name__ == '__main__':
 
+if __name__ == '__main__':
     batch_size = 32
     timesteps = 3
     shape = [162, 5]
     kernel = [162, 2]
     channels = 1
-    filters = 12        # numbers of output channel
+    filters = 12  # numbers of output channel
 
     # Create a placeholder for videos.
     inputs = tf.placeholder(tf.float32, [batch_size, 1, 7, 1])
 
-    multi=resnet(batch_size=32, para=None)
+    multi = resnet(batch_size=32, para=None)
     multi.cnn(inputs)
